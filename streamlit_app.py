00f0b5905937000f312513dd805089f32ba68618
Ganesh Venepally <ganesh.venepally@gmail.com>
2023-06-18 07:43:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,33 @@
 import streamlit as st
+import pprint
+from mftool import Mftool
 import pandas as pd
-import json
 import vectorbt as vbt
+import json
 import quantstats as qs
 import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
-from mftool import Mftool
 
+st.title('Mutual Fund Analysis')
 
+mf = Mftool()
 
+# Get input from user using Streamlit
+Mutual_Fund_Issuer_Name = st.text_input("Enter Mutual Fund Issuer Name", 'uti nifty')
 
-def main():
-
-    st.title("Mutual Fund Analyser")
-    mf = Mftool()
-
-    # Get the input from the user
-    Mutual_Fund_Issuer_Name = st.text_input("Enter Mutual Fund Issuer Name", "uti nifty")
-    Scheme_ID = st.text_input("Enter Scheme ID", "120716")
-
-    # Get the results from the Mftool library
+if Mutual_Fund_Issuer_Name:
     results = mf.get_available_schemes(Mutual_Fund_Issuer_Name)
     st.write(results)
 
-    st.write(mf.get_scheme_details(Scheme_ID))
+# Get input from user using Streamlit
+Scheme_ID = st.text_input("Enter Scheme ID", '120716')
 
-    # Get the data from the vectorbt library
+if Scheme_ID:
+    details = mf.get_scheme_details(Scheme_ID)
+    st.write(details)
+
     data_string = mf.get_scheme_historical_nav(Scheme_ID, as_json=True)
-
     scheme_name = mf.get_scheme_details(Scheme_ID)['scheme_name']
 
     # Parse the string into a dictionary
@@ -48,33 +47,29 @@ def main():
     # Initialize the portfolio by investing the entire cash balance in the asset
     init_cash = 100000  # initial cash in account currency
     size = init_cash / df['nav'].iloc[0]  # number of shares to buy (invest the entire cash balance)
-    price = df['nav'].iloc[0]  # price per share
 
     # Create a vectorbt Portfolio
     portfolio = vbt.Portfolio.from_orders(
         df['nav'],  # price per share
         size,  # size of the order
         init_cash=init_cash,  # initial cash
-        freq='D',  # set frequency to daily
-        price=price,
+        freq='D'  # set frequency to daily
     )
 
     # Calculate daily returns of the portfolio
     returns = portfolio.returns()
 
-    # Display the results
-    st.write(returns)
-
-    # Create a Quantstats report
-    report = qs.reports.html(returns, title='My Portfolio Analysis')
-
-    # Download the report
-    with open('my_portfolio_analysis.html', 'wb') as f:
-        f.write(report.content)
-
-    # Display a download button
-    st.download_button('Download Report', 'my_portfolio_analysis.html')
-
-
-if __name__ == "__main__":
-    main()
+    # Export Quantstats HTML Report
+    qs.reports.html(returns,  title=f"{scheme_name} - VectorBT.html" , output=filepath, download_filename=filepath)
+    
+    st.markdown(get_table_download_link(returns), unsafe_allow_html=True)
+    
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=True)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings
+    href = f'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download csv file</a>'
+    return href
