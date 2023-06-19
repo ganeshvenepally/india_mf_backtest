6ea1276fe653574e3fbe800ebc630e7a4617492c
Ganesh Venepally <ganesh.venepally@gmail.com>
2023-06-19 08:03:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import quantstats as qs
 
-stock = qs.utils.download_returns('META')
+stock = qs.utils.download_returns('AAPL')
+fig = qs.plots.snapshot(stock, title='AAPL Performance', show=False)
+st.write(fig)
